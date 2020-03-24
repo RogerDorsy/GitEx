65b5f80ebe10f3e48ce9309ae2bb1651f90bff27
Fred42-alt <60445090+Fred42-alt@users.noreply.github.com>
2020-03-24 10:06:34 +0100

    Aggiunta classe esterna da istanziare con metodo che stampa

---
+++ GitEx/src/avanzato/Ex2.java
@@ -0,0 +1,9 @@
+package avanzato;
+
+public class Ex2 {
+	
+	public String metodo1(String a, String b) {
+		return (a + "" + b);
+	}
+
+}
--- GitEx/src/base/Ex1.java
+++ GitEx/src/base/Ex1.java
@@ -1,10 +1,14 @@
 package base;
 
+import avanzato.Ex2;
+
 public class Ex1 {
 
 	public static void main(String[] args) {
 		
 		System.out.println(metodo1());
+		Ex2 e2 = new Ex2();
+		System.out.println(e2.metodo1("Ciao da", "Me"));
 
 	}
 	public static String metodo1() {
